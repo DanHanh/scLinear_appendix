62df36106db9ff00b29231a6de1d241fecd9a881
Daniel Hanhart <daniel.hanhart@gmail.com>
2023-07-21 13:25:49 +0200

    21.07.2023

--- scripts/create_environment.R
+++ scripts/create_environment.R
@@ -5,9 +5,12 @@
 #renv::init() ## choose 1: Restore the project from the lockfile.
 
 ## create a venv to beused for the project
-reticulate::virtualenv_create(envname = "scLinear_code" )
-renv::use_python(type = "virtualenv") ## chose the venv to use (path/scLinear_venv)
-reticulate::use_virtualenv(virtualenv = "scLinear_code")
+
+reticulate::virtualenv_create(envname = "./renv/virtualenvs/scLinear_code/bin/python")
+renv::use_python(type = "virtualenv", python = "./renv/virtualenvs/scLinear_code/bin/python/bin/python3.9") ## chose the venv to use (path/scLinear_venv)
+ Sys.setenv(RETICULATE_PYTHON = "./renv/virtualenvs/scLinear_code/bin/python")
+ Sys.unsetenv("RETICULATE_PYTHON")
+reticulate::use_virtualenv(virtualenv = "./renv/virtualenvs/scLinear_code/bin/python", required = TRUE)
 #renv::restore()
 
 
@@ -15,7 +18,7 @@ reticulate::use_virtualenv(virtualenv = "scLinear_code")
 ## donwload zip file from github
 ## extract zip file
 ## install with
-#remove.packages("scLinear")
+remove.packages("scLinear")
 library(devtools)
 devtools::install("./local/scLinear-main", dependencies = TRUE)
 
