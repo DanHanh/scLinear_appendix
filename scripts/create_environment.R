c51756a73baa17efd6f852b6e0d54eb598a82f95
Daniel Hanhart <daniel.hanhart@gmail.com>
2023-10-30 09:31:20 +0100

    mls data set

--- scripts/create_environment.R
+++ scripts/create_environment.R
@@ -6,13 +6,13 @@
 
 ## create a venv to beused for the project
 
-reticulate::virtualenv_create(envname = "./renv/virtualenvs/scLinear_code/bin/python")
-renv::use_python(type = "virtualenv", python = "./renv/virtualenvs/scLinear_code/bin/python/bin/python3.9") ## chose the venv to use (path/scLinear_venv)
- Sys.setenv(RETICULATE_PYTHON = "./renv/virtualenvs/scLinear_code/bin/python")
- Sys.unsetenv("RETICULATE_PYTHON")
-reticulate::use_virtualenv(virtualenv = "./renv/virtualenvs/scLinear_code/bin/python", required = TRUE)
-#renv::restore()
+#reticulate::conda_create(envname =  "./renv/reticulate", python_version = "3.9")
 
+#renv::use_python(type = "conda", python = "./renv/reticulate/bin/python3.9", name = "./renv/reticulate") 
+Sys.setenv(RETICULATE_PYTHON = "./renv/reticulate/bin/python")
+Sys.unsetenv("RETICULATE_PYTHON")
+reticulate::use_condaenv(condaenv = "./renv/reticulate/bin/python", required = TRUE)
+#renv::restore()
 
 ## install scLinear (needs to be downloaded from github)
 ## donwload zip file from github
@@ -20,16 +20,20 @@ reticulate::use_virtualenv(virtualenv = "./renv/virtualenvs/scLinear_code/bin/py
 ## install with
 remove.packages("scLinear")
 library(devtools)
-devtools::install("./local/scLinear-main", dependencies = TRUE)
+#devtools::install("./local/scLinear-main", dependencies = TRUE)
+## directly install from github
+devtools::install_github("DanHanh/scLinear")
+
+## if some packages fail to install try to update the channles
+# reticulate::conda_update()
 
 library(scLinear)
-reticulate::py_module_available("scanpy")
 reticulate::py_module_available("numpy")
 reticulate::py_module_available("sklearn")
 reticulate::py_module_available("joblib")
 reticulate::py_module_available("anndata")
 reticulate::py_module_available("torch")
-
+reticulate::py_module_available("scanpy")
 
 ## print all functions of scLinear
 print(ls("package:scLinear", all.names = TRUE))
